79547671a1041958ca5c9d98e28808ee39a3f06a
Christopher Reyes <christophereyes.cr@gmail.com>
2016-06-30 08:30:49 -0500

    added json object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,17 @@
-$("#main").append("CHRISTOPHER REYES");
+//$("#main").append("CHRISTOPHER REYES");
+var bio = {
+    "name" : "Christopher Reyes",
+    "role" : "student",
+    "contacts" : [
+    {
+        "mobile" : "817-908-4277",
+        "email" : "christophereyes.cr@gmail.com",
+        "github" : "christophereyes"
+    }
+    ],
+    "welcomeMessage" : "Brought to you by : CHRISTOPHER REYES",
+    "skills": ["HTML","CSS","Python","JavaScript"]
+
+}
+
+$("main").append(bio.name);
