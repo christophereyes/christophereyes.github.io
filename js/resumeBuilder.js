d639c26c8cee41492c654c24ab7ecf2a6001b3de
Christopher Reyes <christophereyes.cr@gmail.com>
2016-07-08 08:37:32 -0500

    changed profile picture

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,39 @@
 var bio = {
     "name" : "Christopher Reyes",
     "role" : "student",
-    "contacts" : [
-    {
+    "contacts" : {
         "mobile" : "817-908-4277",
         "email" : "christophereyes.cr@gmail.com",
         "github" : "christophereyes"
-    }
-    ],
+    },
     "welcomeMessage" : "Brought to you by : CHRISTOPHER REYES",
     "skills": ["HTML","CSS","Python","JavaScript"]
 
 }
 
 $(".small2").append(bio.name);
+
+var education = {
+    "schools" : [
+    {"name" : "The University of Texas at Arlington",
+    "location": "Arlington, Texas",
+    "Degree": "Bachelors",
+    "Major": "Computer Science"
+}]
+}
+
+var work = {
+    "jobs" : [
+    {"employer": "AT&T",
+    "title": "Sales Representative",
+    "location": "Hurst, Texas",
+    "Dates": "09/2009 - Current"
+    }]
+}
+
+var projects = {
+    "projects" : [
+    {"title": "Online Portfolio",
+    "Description" : ""
+    }]
+}
